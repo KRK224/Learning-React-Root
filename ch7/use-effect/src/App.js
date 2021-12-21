82a9df146aaf0d912b2d85461366916aa65c12df
KRyun <hihas93@naver.com>
2021-12-21 18:03:33 +0900

    useMemo anyKeyToRender hook 작성

--- ch7/use-effect/src/App.js
+++ ch7/use-effect/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import Checkbox from './components/Checkbox';
 import DependencyArrayTest from './components/DependencyArrayTest'
+import useAnyKeyToRender from './hooks/useAnyKeyToRender';
 
 const App = () =>{
+  useAnyKeyToRender();
+
+  useEffect(()=>{
+    console.log('fresh render');
+  })
   return(
     // <Checkbox />
-    <DependencyArrayTest/>
+    // <DependencyArrayTest/>
+    <h1>Open the console</h1>
   )
 }
 
