6f0061d270fbf853a3b4a27b7a5b88cae3ee448e
KRyun <hihas93@naver.com>
2021-12-21 17:53:33 +0900

    의존 관계 배열 예시 2

--- ch7/use-effect/src/components/DependencyArrayTest.js
+++ ch7/use-effect/src/components/DependencyArrayTest.js
@@ -19,6 +19,14 @@ const DependencyArrayTest = () =>{
     console.log(`saved phrase ${phrase}`);
   }, [phrase]); // 의존 관계 배열 추가
 
+  useEffect(()=>{
+    console.log('either val or phrase has changed');
+  }, [val, phrase]); // 둘다 저장될 경우 실행 => button 클릭시 val 초기화 및 phrase 변경.
+
+  useEffect(()=>{
+    console.log('welcoming message!');
+  }, []) // 의존 관계 배열에 아무것도 없으면 첫 렌더링만 실행
+
   return(
     <>
       <label>Favorite phrase</label>
