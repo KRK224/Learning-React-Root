7fde987b82a85cae634d351aebd837e080755dfc
KRyun <hihas93@naver.com>
2021-12-22 10:23:01 +0900

    memo를 사용한 pureComponent

--- ch7/memo/src/App.js
+++ ch7/memo/src/App.js
@@ -10,14 +10,11 @@ const PureCat = memo(Cat);
 const App = () =>{
   const [cats, setCats] = useState(['Biscuit', 'Jungle', 'Outlaw']);
 
-  useEffect(()=>{
-    console.log(cats);
-  },[cats]);
 
   return(
     <>
       {cats.map((name, i) => {
-        return <Cat key={i} name={name} /> // 추가할 때마다 재 렌더링 됨.
+        return <PureCat key={i} name={name} /> // memo를 사용하여 프롭스 값이 변경 시에만 재렌더링 됨.
       })}
       <button onClick={()=>setCats([...cats, prompt("Name a cat")])}> Add a Cat</button>
     </>
