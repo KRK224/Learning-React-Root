e2f4b74d4289e3b956f19769cbd8d652f8e83b26
KRyun <hihas93@naver.com>
2021-12-22 09:10:58 +0900

    word useEffect 예시 1

--- ch7/use-effect/src/App.js
+++ ch7/use-effect/src/App.js
@@ -1,18 +1,30 @@
 import React, { useEffect } from 'react';
 // import Checkbox from './components/Checkbox';
-import DependencyArrayTest from './components/DependencyArrayTest'
+// import DependencyArrayTest from './components/DependencyArrayTest'
+// import WordCount from './components/WordCount';
 import useAnyKeyToRender from './hooks/useAnyKeyToRender';
 
 const App = () =>{
+  
+  const word = "gnar"; // 원시 값으로서의 word
+  const words = ['sick', 'powder', 'day']
   useAnyKeyToRender();
 
   useEffect(()=>{
-    console.log('fresh render');
-  })
+    console.log('word fresh render');
+  }, [word]); // fresh render가 찍히지 않는다.
+
+  useEffect(()=>{
+    console.log('words fresh render');
+  }, [words]); // 배열은 참조값, 매 렌더링마다 새로 생성한다.
+
   return(
     // <Checkbox />
     // <DependencyArrayTest/>
     <h1>Open the console</h1>
+    // <WordCount>
+    //   You are not going to believe this but...
+    // </WordCount>
   )
 }
 
--- ch7/use-effect/src/components/WordCount.js
+++ ch7/use-effect/src/components/WordCount.js
@@ -1,1 +1,24 @@
+import React, {useEffect} from 'react';
+import useAnyKeyToRender from '../hooks/useAnyKeyToRender';
 
+const WordCount = ({children=""}) =>{
+  
+  const words = children.split(" ");
+
+  useAnyKeyToRender();
+
+  useEffect(()=>{
+    console.log("fresh render");
+  }, [words]);
+
+  return(
+    <>
+      <p>{children}</p>
+      <p>
+        <strong>{words.length} - words</strong>
+      </p>
+    </>  
+  )
+}
+
+export default WordCount;
