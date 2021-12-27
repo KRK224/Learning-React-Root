e7f1603d64b1c7d7dd1ac73ce143a974ffbb5835
KRyun <hihas93@naver.com>
2021-12-27 14:47:56 +0900

    render prop으로 구현한 List component

--- ch8/github-user/src/App.js
+++ ch8/github-user/src/App.js
@@ -1,61 +1,33 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 
-// const loadJSON = key =>
-//   key && JSON.parse(localStorage.getItem(key));
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891},
+  { name: "Monument Peak", elevation: 10067},
+  { name: "Pyramid Peak", elevation: 9983},
+  { name: "Mt. Tallac", elevation: 9735}
+];
 
-// const saveJSON = (key, data) =>
-//   localStorage.setItem(key, JSON.stringify(data));
-
-function GitHubUser({ login }){
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(()=>{
-    
-  }, [data]);
-
-  useEffect(()=>{
-    if(!login) return;
-    setLoading(true);
-        fetch(`https://api.github.com/users/${login}`, {
-        method: "GET",
-        // headers: {
-        //   Authorization: `Bearer ${token}`
-        // }
-      })
-        .then(response => {
-          return response.json();
-        })
-        .then(setData)
-        .then(()=> setLoading(false))
-        .catch(setError)
-  }, 
-  //[login, token]
-  [login])
-
-  if (loading) return <h1>loading...</h1>
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
-  if(!data) return null;
-
-  return (
-    <div className="githubUser">
-      <img
-        src={data.avatar_url} 
-        alt={data.login}
-        style={{ width: 200}}
-      />
-      <div>
-        <h1>{data.login}</h1>
-        {data.name && <p>{data.name}</p>}
-        {data.location && <p>{data.location}</p>}
-      </div>
-    </div>
-  )
+function List({data = [], renderItem, renderEmpty}){
+  return !data.length?
+    ( renderEmpty ): (
+      <ul>
+        {data.map((item, i) => (
+          <li key={i}>{renderItem(item)}</li>
+        ))}
+      </ul>
+    )
 }
 
 export default function App(){
-  return(
-    <GitHubUser login="heedoitdox" />
+  return (
+    <List 
+      renderEmpty={<p>This list is empty</p>} 
+      data={tahoe_peaks}
+      renderItem={item => (
+        <>
+          {item.name} - {item.elevation.toLocaleString()}ft
+        </>
+      )}
+    />
   );
-};
+}
