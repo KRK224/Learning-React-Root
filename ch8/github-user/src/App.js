91f876064e6e80176b314fa07913d044cedbf918
KRyun <hihas93@naver.com>
2021-12-28 10:42:14 +0900

    Fetch 컴포넌트

--- ch8/github-user/src/App.js
+++ ch8/github-user/src/App.js
@@ -1,43 +1,15 @@
-import React from 'react';
-import { FixedSizeList } from 'react-window';
-import faker from 'faker';
-
-const bigList = [...Array(5000)].map(()=>({
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  avatar: faker.internet.avatar()
-}));
-
-function List({data = [], renderItem, renderEmpty}){
-  return !data.length?
-    ( renderEmpty ): (
-      <ul>
-        {data.map((item, i) => (
-          <li key={i}>{renderItem(item)}</li>
-        ))}
-      </ul>
-    )
-}
+import React, { useState } from 'react';
+import GitHubUser from './components/GitHubUser';
+import SearchForm from './components/SearchForm';
 
 export default function App(){
-  
-  const renderRow = ({index, style}) => (
-    <div style={{...style, ...{display: "flex"}}}>
-      <img src={bigList[index].avatar} alt={bigList[index].name} width={50}/>
-      <p>
-        {bigList[index].name} - {bigList[index].email}
-      </p>
-    </div>
-  )
+  const [login, setLogin] = useState("moontahoe");
 
   return (
-    <FixedSizeList
-      height={window.innerHeight}
-      width={window.innerWidth - 20}
-      itemCount={bigList.length}
-      itemSize={50}
-    >
-      {renderRow}
-    </FixedSizeList>
+    <>
+      <SearchForm value={login} onSearch={setLogin} />
+      <GitHubUser login={login}/>
+    </>
+    
   );
 }
---
+++ ch8/github-user/src/components/SearchForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const SearchForm = ({value, onSearch= f=>f}) =>{
+  
+  const [input, setInput] = useState(value); // 책에 따로 코드가 없어서 직접 구현... 
+  // value를 객체화 해서 상태값을 두개로 만드는게 더 나을거 같습니다.
+
+  const submit = e => {
+    e.preventDefault();
+    onSearch(input);
+  }
+  
+  return(
+    <form onSubmit={submit}>
+      <input type="text"
+        placeholder="type User ID..."
+        required
+        value={input}
+        onChange={e=>setInput(e.target.value)}
+      />
+      <button>Search</button>
+    </form>
+  )
+}
+
+export default SearchForm;
